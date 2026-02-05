927337cfcfa9c46d09f06724c00553efc168f8a4
TimmseysTech <timmseystech@gmail.com>
2026-02-05 22:34:36 +1000

    tooling: fix Invoke-DevGuard wrapper (parser-proof)

--- scripts/dev/Invoke-DevGuard.ps1
+++ scripts/dev/Invoke-DevGuard.ps1
@@ -1,9 +1,5 @@
 ﻿Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
-[CmdletBinding()]
-param(
-  [string]$RepoRoot = (Join-Path $HOME 'src\work\Work')
-)
-
+$RepoRoot = Join-Path $HOME 'src\work\Work'
 & (Join-Path $RepoRoot 'scripts\dev\Assert-BaseModelUnchanged.ps1') -RepoRoot $RepoRoot
